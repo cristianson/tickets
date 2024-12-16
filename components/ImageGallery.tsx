8c9c275c265e3c4795152686a4565e45c3679aa4
Cristian <cristian.iordan2@gmail.com>
2024-12-16 22:47:36 +0200

    fix

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
-import Button from './ui/Button'
+import Button from './ui/button'
 
 type ImageData = {
   city: string
