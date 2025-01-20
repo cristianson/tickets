063a1288aa12131647a64fc71362521360e2d5ca
Cristian <cristian.iordan2@gmail.com>
2025-01-20 09:04:32 +0200

    custom shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,38 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['var(--font-inter)'],
+        inter: ["var(--font-inter)"],
       },
       colors: {
         gray: {
-          25: '#FDFDFD',
-          50: '#FAFAFA',
-          100: '#F5F5F5',
-          200: '#E9EAEB',
-          300: '#D5D7DA',
-          400: '#A4A7AE',
-          500: '#717680',
-          600: '#535862',
-          700: '#414651',
-          800: '#252B37',
-          900: '#181D27',
-          950: '#0A0D12',
+          25: "#FDFDFD",
+          50: "#FAFAFA",
+          100: "#F5F5F5",
+          200: "#E9EAEB",
+          300: "#D5D7DA",
+          400: "#A4A7AE",
+          500: "#717680",
+          600: "#535862",
+          700: "#414651",
+          800: "#252B37",
+          900: "#181D27",
+          950: "#0A0D12",
         },
+      },
+      boxShadow: {
+        xsSkeumorphic: `
+          inset 0px 0px 0px 1px rgba(10, 13, 18, 0.18),
+          inset 0px -2px 0px 0px rgba(10, 13, 18, 0.05),
+          0px 1px 2px 0px rgba(16, 24, 40, 0.05)
+        `,
       },
     },
   },
@@ -34,4 +41,4 @@ module.exports = {
   future: {
     hoverOnlyWhenSupported: true,
   },
-} 
+};
