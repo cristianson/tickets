93aa391dbf12581d43b25ea7642bb4d923b6e396
Cristian <cristian.iordan2@gmail.com>
2025-04-04 17:26:47 +0300

    linting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,35 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeToggleButton } from "@/components/ThemeToggleButton";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Train Ticket Gallery',
-  description: 'A gallery showcasing train tickets',
+  title: "Train Ticket Gallery",
+  description: "A gallery showcasing train tickets",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeToggleButton />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
-} 
+  );
+}
