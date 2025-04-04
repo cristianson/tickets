215c583c7f242b4bf04c20cdd4ff56aa7d9b0b48
Cristian <cristian.iordan2@gmail.com>
2025-04-04 17:27:07 +0300

    dark colors for chevron button

--- components/ui/chevronButton.tsx
+++ components/ui/chevronButton.tsx
@@ -17,21 +17,22 @@ const Button = ({
 }: ButtonProps) => {
   const baseStyles = `
     w-9 h-9                                     
-    bg-white                     
+    bg-white dark:bg-gray-800                    
     rounded-full                 
-    border border-gray-200       
+    border border-gray-200 dark:border-gray-600       
     inline-flex                  
     items-center                 
     justify-center               
     transition-all duration-200                  
-    hover:bg-gray-50
+    transition-colors
+    hover:bg-gray-50 dark:hover:bg-gray-700
     active:scale-95
     shadow-xsSkeumorphic
   `;
 
   return (
     <button className={cn(baseStyles, className)} {...props}>
-      <div className="flex items-center justify-center w-[34px] h-[34px] text-gray-600">
+      <div className="flex items-center justify-center w-[34px] h-[34px] text-gray-600 dark:text-gray-100 transition-colors duration-200">
         {variant === "previous" && <ChevronLeft className="h-5 w-5" />}
         {variant === "next" && <ChevronRight className="h-5 w-5" />}
       </div>
